5f3c2ff253c4aa90cb32079be5abbaa9a168037c
Eyad Selmy <eyadselmy1@gmail.com>
2026-08-05 12:51:27 +0100

    Created a basic node project for the frontend (#3)
    * Created a basic node project for the frontend

---
+++ src/routes/jobRouter.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/health", (req, res) => {
+  res.json({ status: "UP", time: new Date().toISOString() });
+});
+
+router.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+export default router;
