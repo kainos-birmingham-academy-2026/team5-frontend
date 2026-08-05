a618344f21b10bcb9ad2fe9bee5e1eeac9ce4755
semiadebayokainos <oluwasemilore.adebayo@kainos.com>
2026-08-05 14:26:14 +0100

    Implemented Biome and Lint

--- src/routes/jobRouter.ts
+++ src/routes/jobRouter.ts
@@ -3,11 +3,11 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/health", (req, res) => {
-  res.json({ status: "UP", time: new Date().toISOString() });
+	res.json({ status: "UP", time: new Date().toISOString() });
 });
 
 router.get("/", (req, res) => {
-  res.send("<h1>Hello World</h1>");
+	res.send("<h1>Hello World</h1>");
 });
 
 export default router;
